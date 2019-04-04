229b171af583af82dc406dff326e3dca69ff6679
unknown <berkelmas96@gmail.com>
2019-04-04 17:49:08 +0300

    ilk commit

---
+++ README.md
@@ -0,0 +1,1 @@
+0'dan react frameworkunu node js olmadan; custom back end ile entegre kullanmak iáin oluüturulmuü react appdir. Bundler olarak webpack; transpiler olarak babel kullançlmçütçr. 
---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <p>Berk Elmas was here... Değiştir...</p>
+      </div>
+    )
+  }
+}
+
+export default App;
